c3db2a9fe3f55386593da6f986871734247a1bd7
ju5td0m7m1nd <franks0059@gmail.com>
2016-03-07 14:46:58 +0800

    ignore jar gz

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 from nltk.parse.stanford import StanfordParser
- 
-dep_parser = StanfordParser('stanford-parser.jar', 'stanford-parser-3.5.2-models.jar')
-input1 = raw_input()
-input2 = raw_input()
-sentences = dep_parser.raw_parse_sents((input1, input2))
+import os
+os.environ['STANFORD_PARSER'] = 'standford-parser.jar'
+os.environ['STANFORD_MODELS'] = 'stanford-parser-3.5.2-models.jar' 
+dep_parser = StanfordParser(model_path='englishPCFG.ser.gz')
+s = 'how to describe beach'
+sentences = dep_parser.raw_parse(s)
 
 a = iter(sentences)
 
 for i in a:
     b = iter(i)
     for j in b:
-        print j
+        print (j)
 
