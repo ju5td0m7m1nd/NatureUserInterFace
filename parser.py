5c9c3691b9cf0b6699ccce59035a8bb93d052cb8
ju5td0m7m1nd <franks0059@gmail.com>
2016-03-27 21:00:36 +0800

    create FeatureParser class

--- parser.py
+++ parser.py
@@ -10,43 +10,59 @@ os.environ['STANFORD_MODELS'] = './stanford-parser/stanford-parser-3.5.2-models.
 dep_parser = StanfordParser(model_path='./stanford-parser/englishPCFG.ser.gz')
 
 FILENAME = 'feature.data'
-sentence_level = ['S','SBAR','SBARQ','SINV','SQ']
-phrase_level = ['ADJP','ADVP','CONJP','FRAG','INTJ','LST','NAC','NP','NX','PP','PRN','PRT','QP','RRC','UCP','VP',
-                'WHADJP','WHAVP','WHNP','WHPP','X']
-word_level=['CC','CD','DT','EX','FW','IN','JJ','JJR','JJS','LS','MD','NN','NNS','NNP','NNPS','PDT','POS','PRP','PRP$','RB',
-            'RBR','RBS','RP','SYM','TO','UH','VB','VBD','VBG','VBN','VBP','VBZ','WDT','WP','WP$','WRB']
 
-tagList = {'PRP$': 2, 'FW': 4, 'WP': 5, 'VBN': 1, 'POS': 1, 'VBP': 1, 'WDT': 1, 'JJ': 1, 'VBZ': 1, 'VBG': 1, 'DT': 1, 'RP': 1, '$': 1, 'NN': 1, 'RBR': 1, 'VBD': 1, ',': 1, '.': 1, 'TO': 1, 'LS': 1, 'RB': 1, 'NNS': 1, 'NNP': 1, 'VB': 1, 'WRB': 1, 'CC': 1, 'PDT': 1, 'RBS': 1, 'PRP': 1, 'CD': 1, 'EX': 1, 'IN': 1, 'WP$': 1, 'MD': 1, 'NNPS': 1, 'JJS': 1, 'JJR': 1, 'SYM': 1, 'UH': 1}
+class FeatureParser():
 
-f = open('questionary.csv', 'r')  
-Question =[] 
-for row in csv.reader(f):
-    Question.append(row[3:])
+    sentenceLevel = []
+    phraseLevel = []
+    wordLevel = []
+    questionFeature = []   
+ 
+    def InitData(self): 
+        self.sentenceLevel = ['S','SBAR','SBARQ','SINV','SQ']
+        self.phraseLevel = ['ADJP','ADVP','CONJP','FRAG','INTJ','LST','NAC','NP','NX','PP','PRN','PRT','QP','RRC','UCP','VP',
+                        'WHADJP','WHAVP','WHNP','WHPP','X']
+        self.wordLevel=['CC','CD','DT','EX','FW','IN','JJ','JJR','JJS','LS','MD','NN','NNS','NNP','NNPS','PDT','POS','PRP','PRP$','RB',
+                    'RBR','RBS','RP','SYM','TO','UH','VB','VBD','VBG','VBN','VBP','VBZ','WDT','WP','WP$','WRB']
 
+    def ReadDataFromFile(self):
+        f = open('questionary.csv', 'r')  
+        Question =[] 
+        for row in csv.reader(f):
+            Question.append(row[3:])
+        QuestionFeature = []
+        for q in Question[1:]:
+            try:
+                questionParsed = dep_parser.raw_parse(q[0])
+                for q in questionParsed:
+                    feature = {}
+                    current_label = '';
+                    for leaves in q.subtrees():
+                        label = leaves.label()
+                        if label == 'ROOT':
+                             continue
+                        elif label in sentence_level:
+                            feature[label] = []
+                            current_label = label
+                        else :
+                            feature[current_label].append(label)
+                    QuestionFeature.append(feature)
+            except:
+                continue
 
-QuestionFeature = []
+        f = open(FILENAME,'wb')
+        json.dump(QuestionFeature,f)
+        f.close()
+    def ReadDataFromJson(self):
+        Feature_json = open(FILENAME).read()
+        self.questionFeature = json.loads(Feature_json) 
+    def __PrintData__(self):
+        
+        print self.questionFeature
 
-for q in Question[1:]:
-    try:
-        questionParsed = dep_parser.raw_parse(q[0])
-        for q in questionParsed:
-            feature = {}
-            current_label = '';
-            for leaves in q.subtrees():
-                label = leaves.label()
-                print label 
-                if label == 'ROOT':
-                     continue
-                elif label in sentence_level:
-                    feature[label] = []
-                    current_label = label
-                else :
-                    feature[current_label].append(label)
-            QuestionFeature.append(feature)
-    except:
-        continue
+FP = FeatureParser()
+FP.ReadDataFromJson()
 
-f = open(FILENAME,'wb')
-json.dump(QuestionFeature,f)
-f.close()
-print len(QuestionFeature)
+for f in FP.questionFeature:
+    print f
+
