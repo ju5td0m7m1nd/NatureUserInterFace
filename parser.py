13cbb45bb413edb6d3bc4119f30c8be7d2b48ad4
ju5td0m7m1nd <franks0059@gmail.com>
2016-03-27 22:08:27 +0800

    add print production, pos function

--- parser.py
+++ parser.py
@@ -11,58 +11,77 @@ dep_parser = StanfordParser(model_path='./stanford-parser/englishPCFG.ser.gz')
 
 FILENAME = 'feature.data'
 
-class FeatureParser():
+'''
+self attribute
+@question
+@questionParsed
+@questionFeature
+@sentenceLevel
+@phraseLevel
+@wordLevel
 
-    sentenceLevel = []
-    phraseLevel = []
-    wordLevel = []
-    questionFeature = []   
+'''
+class FeatureParser():
  
-    def InitData(self): 
+    def __init__(self): 
         self.sentenceLevel = ['S','SBAR','SBARQ','SINV','SQ']
         self.phraseLevel = ['ADJP','ADVP','CONJP','FRAG','INTJ','LST','NAC','NP','NX','PP','PRN','PRT','QP','RRC','UCP','VP',
                         'WHADJP','WHAVP','WHNP','WHPP','X']
         self.wordLevel=['CC','CD','DT','EX','FW','IN','JJ','JJR','JJS','LS','MD','NN','NNS','NNP','NNPS','PDT','POS','PRP','PRP$','RB',
                     'RBR','RBS','RP','SYM','TO','UH','VB','VBD','VBG','VBN','VBP','VBZ','WDT','WP','WP$','WRB']
-
-    def ReadDataFromFile(self):
+        self.ReadDataFromJson()
+        self.ReadQuestionFromFile()
+        self.ParseData()
+ 
+    def ReadQuestionFromFile(self):
         f = open('questionary.csv', 'r')  
-        Question =[] 
+        question =[] 
         for row in csv.reader(f):
-            Question.append(row[3:])
-        QuestionFeature = []
-        for q in Question[1:]:
+            question.append(row[3:])
+        self.question = question
+        f.close() 
+   
+    def ParseData(self):
+        questionParsed = []
+        print('Parsing.')
+        for q in self.question[1:3]:
             try:
-                questionParsed = dep_parser.raw_parse(q[0])
-                for q in questionParsed:
-                    feature = {}
-                    current_label = '';
-                    for leaves in q.subtrees():
-                        label = leaves.label()
-                        if label == 'ROOT':
-                             continue
-                        elif label in sentence_level:
-                            feature[label] = []
-                            current_label = label
-                        else :
-                            feature[current_label].append(label)
-                    QuestionFeature.append(feature)
+                print '.'
+                question = dep_parser.raw_parse(q[0])
+                questionParsed.append(question)
             except:
                 continue
-
+        self.questionParsed = questionParsed 
+    def SaveToJson(self):  
+        QuestionFeature = []  
+        for q in self.questionParsed:
+             feature = {}
+             current_label = '';
+             for leaves in q.subtrees():
+                 label = leaves.label()
+                 if label == 'ROOT':
+                      continue
+                 elif label in sentence_level:
+                     feature[label] = []
+                     current_label = label
+                 else :
+                     feature[current_label].append(label)
+             QuestionFeature.append(feature)
         f = open(FILENAME,'wb')
         json.dump(QuestionFeature,f)
         f.close()
     def ReadDataFromJson(self):
         Feature_json = open(FILENAME).read()
         self.questionFeature = json.loads(Feature_json) 
-    def __PrintData__(self):
-        
-        print self.questionFeature
+    def __PrintProduction__(self):
+        for q in self.questionParsed:
+            for tree in list(q):
+                print tree.productions()
+    def __PrintPos__(self):
+        for q in self.questionParsed:
+            for tree in list(q):
+                print tree.pos()
 
-FP = FeatureParser()
-FP.ReadDataFromJson()
-
-for f in FP.questionFeature:
-    print f
-
+if __name__ == "__main__":
+    FP = FeatureParser()
+    FP.__PrintProduction__()
