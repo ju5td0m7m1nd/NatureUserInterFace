4c26e669dc644e86f2d6470258ced892fe49186e
ju5td0m7m1nd <franks0059@gmail.com>
2016-03-29 15:30:33 +0800

    working on binary classify

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import nltk
 from nltk.parse.stanford import StanfordParser
+import matplotlib.pyplot as plt
 import json
 import os
 import csv
@@ -27,12 +28,12 @@ self attribute
 '''
 class FeatureParser():
  
-    def __init__(self,filename,PARSEDATA):
-        self.fileName = filename 
+    def __init__(self,filename,PARSEDATA,command):
+        self.fileName = filename
+        self.command = command 
         self.sentenceLevel = ['S','SBAR','SBARQ','SINV','SQ']
         self.phraseLevel = ['ADJP','ADVP','CONJP','FRAG','INTJ','LST','NAC','NP','NX','PP','PRN','PRT','QP','RRC','UCP','VP','WHADJP','WHAVP','WHNP','WHPP','X']
         self.wordLevel=['CC','CD','DT','EX','FW','IN','JJ','JJR','JJS','LS','MD','NN','NNS','NNP','NNPS','PDT','POS','PRP','PRP$','RB','RBR','RBS','RP','SYM','TO','UH','VB','VBD','VBG','VBN','VBP','VBZ','WDT','WP','WP$','WRB']
-        self.ReadDataFromJson()
         self.ReadQuestionFromFile()
         '''
         if user just need to use the feature,
@@ -40,7 +41,8 @@ class FeatureParser():
         '''
         if PARSEDATA:
             self.ParseData()
- 
+            self.SaveToJson()
+        self.ReadDataFromJson()
     def ReadQuestionFromFile(self):
         f = open(self.fileName, 'r')  
         question =[] 
@@ -49,13 +51,14 @@ class FeatureParser():
         self.question = question
         f.close() 
    
+    # different colums in self.question
     def ParseData(self):
         questionParsed = []
         print('Parsing.')
-        for q in self.question[1:]:
+        for q in self.question[1:30]:
             try:
                 print '.'
-                question = dep_parser.raw_parse(q[0])
+                question = dep_parser.raw_parse(q[self.command])
                 questionParsed.append(question)
             except:
                 continue
@@ -64,29 +67,51 @@ class FeatureParser():
         questionFeature = []  
         for q in self.questionParsed:
              feature = {}
+             for question in q:
+                q = question
              current_label = '';
              for leaves in q.subtrees():
                  label = leaves.label()
                  if label == 'ROOT':
                       continue
-                 elif label in sentence_level:
+                 elif label in self.sentenceLevel:
                      feature[label] = []
                      current_label = label
                  else :
-                     feature[current_label].append(label)
+                     if current_label == '':
+                        continue
+                     else: 
+                        feature[current_label].append(label)
              questionFeature.append(feature)
-        f = open('feature.data','wb')
+        f = open('feature'+str(self.command)+'.data','wb')
         json.dump(questionFeature,f)
         f.close()
     def ReadDataFromJson(self):
-        Feature_json = open('feature.data').read()
+        Feature_json = open('feature'+str(self.command)+'.data').read()
         self.questionFeature = json.loads(Feature_json) 
     
+    def CalculateLayer(self):
+        layer = []
+        for q in self.questionFeature:
+            layer.append(len(q))
+        self.layer = layer   
+ 
+    def CalculatePhraseLevel(self):
+        phraseLevel = []
+        for feature in self.questionFeature:
+            count = 0
+            for key in feature: 
+                for f in feature[key]:
+                    if f in self.phraseLevel:
+                       count = count +1
+            phraseLevel.append(count)
+            self.phraseCount = phraseLevel
+
     def MakeFeature(self):
         for feature in self.questionFeature: 
             for key in feature:
-                print key 
-
+                print key
+                print "    "+str(feature[key])
     def __PrintProduction__(self):
         for q in self.questionParsed:
             for tree in list(q):
@@ -95,10 +120,24 @@ class FeatureParser():
         for q in self.questionParsed:
             for tree in list(q):
                 print tree.pos()
-
+    def __PrintQuestionParsed__(self):
+        for q in self.questionParsed:
+            for t in list(q):
+                print t    
 if __name__ == "__main__":
-    FP = FeatureParser('questionary.csv',False)
-    FP.MakeFeature()
+    
+    FP2 = FeatureParser('questionary.csv',True,2)
+    FP2.CalculateLayer()
+    FP2.CalculatePhraseLevel()
+    
+    FP4 = FeatureParser('questionary.csv',True,4)
+    FP4.CalculateLayer()
+    FP4.CalculatePhraseLevel()
+     
+    plt.scatter(FP2.phraseCount,FP2.layer,color="r")
+    plt.scatter(FP4.phraseCount,FP4.layer,color="g")
+    
+    plt.show()
     # test for print production
     #FP.__PrintProduction__()
     # test for print pos
