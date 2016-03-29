cd0461d945ba7ee0bff45decfe595ca0c1aa95b6
ju5td0m7m1nd <franks0059@gmail.com>
2016-03-29 13:49:20 +0800

    modified parser

--- parser.py
+++ parser.py
@@ -30,10 +30,8 @@ class FeatureParser():
     def __init__(self,filename,PARSEDATA):
         self.fileName = filename 
         self.sentenceLevel = ['S','SBAR','SBARQ','SINV','SQ']
-        self.phraseLevel = ['ADJP','ADVP','CONJP','FRAG','INTJ','LST','NAC','NP','NX','PP','PRN','PRT','QP','RRC','UCP','VP',
-                        'WHADJP','WHAVP','WHNP','WHPP','X']
-        self.wordLevel=['CC','CD','DT','EX','FW','IN','JJ','JJR','JJS','LS','MD','NN','NNS','NNP','NNPS','PDT','POS','PRP','PRP$','RB',
-                    'RBR','RBS','RP','SYM','TO','UH','VB','VBD','VBG','VBN','VBP','VBZ','WDT','WP','WP$','WRB']
+        self.phraseLevel = ['ADJP','ADVP','CONJP','FRAG','INTJ','LST','NAC','NP','NX','PP','PRN','PRT','QP','RRC','UCP','VP','WHADJP','WHAVP','WHNP','WHPP','X']
+        self.wordLevel=['CC','CD','DT','EX','FW','IN','JJ','JJR','JJS','LS','MD','NN','NNS','NNP','NNPS','PDT','POS','PRP','PRP$','RB','RBR','RBS','RP','SYM','TO','UH','VB','VBD','VBG','VBN','VBP','VBZ','WDT','WP','WP$','WRB']
         self.ReadDataFromJson()
         self.ReadQuestionFromFile()
         '''
@@ -63,7 +61,7 @@ class FeatureParser():
                 continue
         self.questionParsed = questionParsed 
     def SaveToJson(self):  
-        QuestionFeature = []  
+        questionFeature = []  
         for q in self.questionParsed:
              feature = {}
              current_label = '';
@@ -76,13 +74,19 @@ class FeatureParser():
                      current_label = label
                  else :
                      feature[current_label].append(label)
-             QuestionFeature.append(feature)
+             questionFeature.append(feature)
         f = open('feature.data','wb')
-        json.dump(QuestionFeature,f)
+        json.dump(questionFeature,f)
         f.close()
     def ReadDataFromJson(self):
         Feature_json = open('feature.data').read()
         self.questionFeature = json.loads(Feature_json) 
+    
+    def MakeFeature(self):
+        for feature in self.questionFeature: 
+            for key in feature:
+                print key 
+
     def __PrintProduction__(self):
         for q in self.questionParsed:
             for tree in list(q):
@@ -94,6 +98,7 @@ class FeatureParser():
 
 if __name__ == "__main__":
     FP = FeatureParser('questionary.csv',False)
+    FP.MakeFeature()
     # test for print production
     #FP.__PrintProduction__()
     # test for print pos
