776ecb08949d0797c5e9490a284945e5d60c910c
ju5td0m7m1nd <franks0059@gmail.com>
2016-05-05 14:55:03 +0800

    push latest

--- WordRelation.py
+++ WordRelation.py
@@ -13,5 +13,28 @@ if find in secord level , the score will be [describe's score] * 0.8, and soon.
 '''
 WRC stands for word relation calculator
 '''
-class WRC():
+from nltk.corpus import wordnet as wn
 
+class WRC():
+    
+    def __init__(self):
+        self.level = 0
+        pass
+
+    def findLemma(self,word,pos,level,target):
+        if level == 5:
+            return False
+        synsetName = word+'.'+pos+'.01'
+        for word in wn.synset(synsetName)._lemma_names:
+            print word +' '+target
+            if word == target : 
+                return level
+            #print str(level) +' '+word
+            self.findLemma(word,'v',level+1,target) 
+
+
+if __name__ == '__main__':
+    words = ['describe']
+    for word in words :
+        w = WRC()
+        print w.findLemma(word,'v',0,'elaborate') 
