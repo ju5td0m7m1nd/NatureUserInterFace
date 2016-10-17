00457a831d5e875cb45d2492efb9513f650c99be
hellosirandy <sac840711@gmail.com>
2016-10-17 12:03:41 +0800

    complete rearangeing code

--- MainApp.py
+++ MainApp.py
@@ -1,45 +1,20 @@
-#from FeatureExtractor import FeatureExtractor
-from ClassifiedManager import ClassifiedManager
-import pickle
-import os
 from nltk.corpus import wordnet as wn
-
-from QuestionTypeTools import FeatureExtractor
-
-
+from QuestionTypeTools import FeatureExtractor, Predictor, QueryManager
 
 class MainApp():
     def __init__(self):
-        #self.KE = KeywordExtractor()
         self.wn = wn.synsets('describe','v')
         self.FE = FeatureExtractor()
         print "Main App: Feature Extractor init"
-        MODEL_PATH = ''
-        if 'NatureUserInterface' in os.environ['PWD']:
-          MODEL_PATH = './'
-        else :
-          MODEL_PATH = os.path.abspath(os.path.dirname(__name__)) + '/Main/NatureUserInterface/'
-        self.loaded_model = pickle.load(open(MODEL_PATH+'model.sav', 'rb'))
-        print "Main App: Model loaded "
-        self.CM = ClassifiedManager()
+        self.QM = QueryManager()
         print "Main App: Classified Manager init"
-    def Input(self, question):
-        #try:
-        feature = [self.FE.GetFeature(question, wn)]
-        typeOfQuestion = self.loaded_model.predict(feature)[0]
-        return {
-          'parse': True,
-          'command': self.CM.Classify(typeOfQuestion, question, self.FE.GetKeyword())
-        }
-        '''
-        except:
-            # Default command _ _ keyword _ _
-            # We should predict a most related command to user
-            return {
-              'parse': False,
-              'command': '_ _ '+ self.FE.GetKeyword()[0] + ' _ _'
-            }
-        '''
+        self.predictor = Predictor()
+        print "Main App: Predictor init"
+
+    def Input(self, inputQuestion):
+        features = [self.FE.GetFeature(inputQuestion, wn)]
+        questionType = self.predictor.Predict(features)
+        return self.QM.GetQuery(questionType, inputQuestion, self.FE.GetKeyword())
 
 if __name__ == "__main__":
     mainApp = MainApp()
--- QuestionTypeTools.py
+++ QuestionTypeTools.py
@@ -1,5 +1,7 @@
 from KeywordExtractor import *
 from ExtractFeatureTools import NearestFinder, WordRelationCounter
+import pickle
+import os
 
 PARSER_PATH = ''
 if 'NatureUserInterface' in os.environ['PWD']:
@@ -48,3 +50,76 @@ class FeatureExtractor:
 
     def GetKeyword(self):
         return self.keyword
+
+class Predictor:
+    def __init__(self):
+        self.model = self.LoadModel()
+    def LoadModel(self):
+        modelPath = ''
+        if 'NatureUserInterface' in os.environ['PWD']:
+          modelPath = './'
+        else :
+          modelPath = os.path.abspath(os.path.dirname(__name__)) + '/Main/NatureUserInterface/'
+        return pickle.load(open(modelPath+'model.sav', 'rb'))
+    def Predict(self, features):
+        return self.model.predict(features)[0]
+
+class QueryManager:
+    #def __init__(self):
+    def GetQuery(self, questionType, inputQuestion, keyword):
+        result = {'parse': True, 'command': ''}
+        try:
+            if questionType == 0:
+                result['command'] = self.HowToUse(keyword)
+            elif questionType == 1:
+                result['commmand'] = self.WhichIsRight(inputQuestion, keyword)
+            elif questionType == 2:
+                result['command'] = self.AddPartOfSpeech(inputQuestion, keyword)
+            elif questionType == 3:
+                result['command'] = self.ReplaceWord(inputQuestion, keyword)
+        except:
+            result['parse'] = False
+        return result
+
+    def HowToUse(self, keyword):
+        query = '_ _ ' + keyword[0] + ' _ _'
+        return query
+
+    def WhichIsRight(self, inputQuestion, keyword):
+        query = ''
+        keyword1 = keyword[0].split()
+        keyword2 = keyword[1].split()
+        #in/at the afternoon
+        if len(keyword1) == len(keyword2):
+            for i in range(0, len(keyword1)):
+                if keyword1[i] != keyword2[i]:
+                    query += keyword1[i] + "/" + keyword2[i] + " "
+                else:
+                    query += keyword1[i] + " "
+        #listen ?to music
+        else:
+            if len(keyword1) > len(keyword2):
+                for word in keyword1:
+                    if word not in keyword2:
+                        query += "?"
+                    query += word + " "
+            else:
+                for word in keyword2:
+                    if word not in keyword1:
+                        query += "?"
+                    query += word + " "
+        return query
+
+    def AddPartOfSpeech(self, inputQuestion, keyword):
+        query = 'adj. ' + keyword[0]
+        return query
+
+    def ReplaceWord(self, inputQuestion, keyword):
+        query = ''
+        target = keyword[0] if len(keyword[0]) < len(keyword[1]) else keyword[1]
+        sentence = keyword[1] if len(keyword[0] < len(keyword[1])) else keyword[1]
+        for word in sentence.split():
+            if target == word:
+                query += "~"
+            query += word + " "
+        return query
