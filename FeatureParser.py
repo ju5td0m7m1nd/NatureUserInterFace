6abdc49ea257797ad6c299367e342a2590c5e45c
Frank Tsai <franks0059@gmail.com>
2016-11-08 21:01:08 +0800

    keyword

--- FeatureParser.py
+++ FeatureParser.py
@@ -38,87 +38,6 @@ class FeatureParser():
             self.ReadQuestionFromFile()
             self.ParseData()
             self.SaveToJson()
-        elif dataType == 'test':
-            self.ReadQuestionFromFile()
-            self.GenerateTestData()
-            self.SaveTestData()
-        elif dataType == 'correct':
-            self.ReadQuestionFromFile()
-            self.GenerateCorrectAnswer()
-            self.SaveCorrectAnswer()
-    def GenerateTestData(self):
-        questionParsed = []
-        print('Parsing For Test' + str(self.command))
-        question_length = len(self.question)
-        for q in self.question[(question_length/10) * 7: question_length]:
-            try:
-                question = self.dep_parser.raw_parse(q[self.command])
-                questionParsed.append(question)
-            except:
-                continue
-        self.questionParsed = questionParsed
-
-    def SaveTestData(self):
-        questionFeature = []
-        fTEXT = open('./static/keyword/TestData/test_data' + str(self.command) + '.txt', 'wb')
-        for q in self.questionParsed:
-             feature = []
-             for question in q:
-                q = question
-                KEYWORD_SYMBOL = False
-             for leave in q.subtrees(lambda t: t.height() == 2):
-                #print leave[0]
-                if (leave[0] == '\''):
-                  KEYWORD_SYMBOL = not KEYWORD_SYMBOL
-                elif (leave[0] == '`'):
-                  KEYWORD_SYMBOL = not KEYWORD_SYMBOL
-                else :
-                  if (KEYWORD_SYMBOL):
-                    keyword_label = 'T'
-                  else :
-                    keyword_label = 'F'
-                  feature.append(leave[0] + '   ' + leave.label() +'\n')
-                  fTEXT.write(leave[0] + '    ' + leave.label() +'\n')
-             questionFeature.append(feature)
-             fTEXT.write('\n')
-        fTEXT.close()
-
-    def GenerateCorrectAnswer(self):
-        questionParsed = []
-        print('Parsing For Test' + str(self.command))
-        question_length = len(self.question)
-        for q in self.question[(question_length/10) * 7: question_length]:
-            try:
-                question = self.dep_parser.raw_parse(q[self.command])
-                questionParsed.append(question)
-            except:
-                continue
-        self.questionParsed = questionParsed
-
-    def SaveCorrectAnswer(self):
-        questionFeature = []
-        fTEXT = open('./static/keyword/TestData/correct_data' + str(self.command) + '.txt', 'wb')
-        for q in self.questionParsed:
-             feature = []
-             for question in q:
-                q = question
-                KEYWORD_SYMBOL = False
-             for leave in q.subtrees(lambda t: t.height() == 2):
-                #print leave[0]
-                if (leave[0] == '\''):
-                  KEYWORD_SYMBOL = not KEYWORD_SYMBOL
-                elif (leave[0] == '`'):
-                  KEYWORD_SYMBOL = not KEYWORD_SYMBOL
-                else :
-                  if (KEYWORD_SYMBOL):
-                    keyword_label = 'T'
-                  else :
-                    keyword_label = 'F'
-                  feature.append(leave[0] + '   ' + leave.label() +'\n')
-                  fTEXT.write(leave[0] + '    ' + leave.label() + '   '+ keyword_label + '\n')
-             questionFeature.append(feature)
-             fTEXT.write('\n')
-        fTEXT.close()
 
 
     def ParseSentence(self, question):
@@ -157,30 +76,36 @@ class FeatureParser():
                 #question = dep_parser.raw_parse(q[self.command])
                 text = word_tokenize(q[self.command])
                 question = pos_tag(text)
-                questionParsed.append(question)
+                questionParsed.append({'tokenize': question, 'original': q[self.command]})
             except:
                 continue
         self.questionParsed = questionParsed
     def SaveToJson(self):
         questionFeature = []
-        fTEXT = open('./static/keyword/TrainData/v2/train_data' + str(self.command) + '.txt', 'wb')
+        fTEXT = open('./static/keyword/TrainData/train_data' + str(self.command) + '.txt', 'wb')
         for q in self.questionParsed:
-          feature = []
-          KEYWORD_SYMBOL = False
-          print q
-          for word_pos in q:
+          feature = {} 
+          KEYWORD_SYMBOL = 'F' 
+          '''
+          Parse sentence into object with the datatype
+          {`someword`: `flag`} 
+          for example: {'how': 'F', 'to': 'F', 'describe': 'F', 'beach': 'T'} 
+          '''
+
+          for word_pos in q['tokenize']:
+            feature[word_pos[0]] = KEYWORD_SYMBOL 
             if (word_pos[0] == '``'):
-              KEYWORD_SYMBOL = True
+              KEYWORD_SYMBOL = 'T' 
             elif (word_pos[0] == '\'\''):
-              KEYWORD_SYMBOL = False
-            else:
-              if (KEYWORD_SYMBOL):
-                keyword_label = 'T'
-                print(word_pos[0] + '    '+ word_pos[1] + '    ' + keyword_label)
-                fTEXT.write(word_pos[0] + '    '+ word_pos[1] + '    ' + keyword_label + '\n')
-              else:
-                keyword_label = 'F'
-                fTEXT.write(word_pos[0] + '    '+ word_pos[1] + '    ' + keyword_label + '\n')
+              KEYWORD_SYMBOL = 'F' 
+          split_question = q['original'].split('"')
+          combine_question = ''
+          for split_q in split_question:
+            combine_question += split_q
+          words_pos = pos_tag(word_tokenize(combine_question)) 
+          for w in words_pos:
+            fTEXT.write(w[0] + '    '+ w[1] + '    ' + feature[w[0]] + '\n')
+          
           fTEXT.write('\n')
         '''
         for q in self.questionParsed:
@@ -204,41 +129,22 @@ class FeatureParser():
              questionFeature.append(feature)
              fTEXT.write('\n')
         '''
-        #fJSON = open('./features/command'+str(self.command)+'.json','wb')
-        #json.dump(questionFeature,fJSON)
-        #fJSON.close()
         fTEXT.close()
-        print questionFeature
 
 
 if __name__ == "__main__":
     #FP = FeatureParser('./static/keywordRawInput.csv', 'train', 0)
     #FP = FeatureParser('./static/keywordRawInput.csv', 'train', 1)
-    #FP = FeatureParser('./static/keywordRawInput.csv', 'train', 2)
+    FP = FeatureParser('./static/keywordRawInput.csv', 'train', 2)
     #FP = FeatureParser('./static/keywordRawInput.csv', 'train', 3)
     #FP = FeatureParser('./static/keywordRawInput.csv', 'train', 4)
     #FP = FeatureParser('./static/keywordRawInput.csv', 'train', 5)
     #FP = FeatureParser('./static/keywordRawInput.csv', 'train', 6)
+    '''
     FP = FeatureParser('./static/keywordRawInput_v2.csv', 'train', 0)
     FP = FeatureParser('./static/keywordRawInput_v2.csv', 'train', 1)
     FP = FeatureParser('./static/keywordRawInput_v2.csv', 'train', 2)
     FP = FeatureParser('./static/keywordRawInput_v2.csv', 'train', 3)
     FP = FeatureParser('./static/keywordRawInput_v2.csv', 'train', 4)
     FP = FeatureParser('./static/keywordRawInput_v2.csv', 'train', 5)
-    #FP = FeatureParser('./static/keywordRawInput.csv', 'test', 0)
-    #FP = FeatureParser('./static/keywordRawInput.csv', 'test', 1)
-    #FP = FeatureParser('./static/keywordRawInput.csv', 'test', 2)
-    #FP = FeatureParser('./static/keywordRawInput.csv', 'test', 3)
-    #FP = FeatureParser('./static/keywordRawInput.csv', 'test', 4)
-    #FP = FeatureParser('./static/keywordRawInput.csv', 'test', 5)
-    #FP = FeatureParser('./static/keywordRawInput.csv', 'test', 6)
-
-    '''
-    FP = FeatureParser('./static/keywordRawInput.csv', 'correct', 0)
-    FP = FeatureParser('./static/keywordRawInput.csv', 'correct', 1)
-    FP = FeatureParser('./static/keywordRawInput.csv', 'correct', 2)
-    FP = FeatureParser('./static/keywordRawInput.csv', 'correct', 3)
-    FP = FeatureParser('./static/keywordRawInput.csv', 'correct', 4)
-    FP = FeatureParser('./static/keywordRawInput.csv', 'correct', 5)
-    FP = FeatureParser('./static/keywordRawInput.csv', 'correct', 6)
     '''
